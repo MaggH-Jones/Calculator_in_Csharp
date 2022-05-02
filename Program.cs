032989f757de7823896924e43d30bae01a22488f
MaggH-Jones <102585564+MaggH-Jones@users.noreply.github.com>
2022-05-02 22:46:08 +0200

    added if starts

--- Program.cs
+++ Program.cs
@@ -9,13 +9,31 @@ class Program
         string choice;
         int newNumber;
         int replaceNumber;
-        int answer = num01;
+        int answer;
         bool whenToEnd = false;
 
         Console.WriteLine("Type in your first number");
         firstNumber = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("What will you do now?\n.+\n.-\n.*\n./");
-        if 
+        choice = Console.ReadLine();
+        if (choice == "+")
+        {
+        }
+        else if (choice == "-")
+        {
+        }
+        else if (choice == "*")
+        {
+        }
+        else if (choice == "/")
+        {
+        }
+        else
+        {
+            Console.WriteLine(firstNumber);
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
 
         while (whenToEnd = true){
         Console.WriteLine("Type in your next number");
