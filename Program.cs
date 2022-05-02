9b8ff976b924a48402f48a8ca43278c50a09e62b
MaggH-Jones <102585564+MaggH-Jones@users.noreply.github.com>
2022-05-02 22:11:34 +0200

    fixing erros

--- Program.cs
+++ Program.cs
@@ -5,14 +5,19 @@ class Program
 {
     static void Main(string[] args)
     {
+        int number;
+        int replaceNumber;
+        int answer = num01;
+
         Console.WriteLine("Hello, World!");
+
         Console.ReadKey();
     }
 }
 
 public class calculator
 {
-    public int plus()
+    public int cal(int num01, int num02)
     {
         int answer;
 
