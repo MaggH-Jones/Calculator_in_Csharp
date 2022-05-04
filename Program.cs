d223752df240c3b36741adf00d0855cf028df8f9
MaggH-Jones <102585564+MaggH-Jones@users.noreply.github.com>
2022-05-04 11:24:12 +0200

    added a way to end, and the calculator works now

--- Program.cs
+++ Program.cs
@@ -18,6 +18,7 @@ class Program
         choice = Console.ReadLine();
         if (choice == "+")
         {
+            
         }
         else if (choice == "-")
         {
@@ -35,17 +36,50 @@ class Program
             Environment.Exit(0);
         }
 
-        while (whenToEnd = true)
+        Console.WriteLine("Will you add anouther number?");
+        string quit = Console.ReadLine();
+        if (quit == "yes")
         {
+            whenToEnd = true;
+        }
+
+        while (whenToEnd != true)
+        {
+            
             Console.WriteLine("Type in your next number");
             newNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Will you add anouther number?");
+            quit = Console.ReadLine();
+            if (quit == "yes")
+            {
+                whenToEnd = true;
+            }
         }
 
         Console.ReadKey();
     }
-    public int cal(int num01, int num02)
+    public int plus(int num01, int num02)
     {
-        int answer = 1;
+        int answer = num01 + num02;
+
+        return answer;
+    }
+    public int minus(int num01, int num02)
+    {
+        int answer = num01 - num02;
+
+        return answer;
+    }
+    public int gange(int num01, int num02)
+    {
+        int answer = num01 * num02;
+
+        return answer;
+    }
+    public int divider(int num01, int num02)
+    {
+        int answer = num01 / num02;
 
         return answer;
     }
